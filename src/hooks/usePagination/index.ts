341c8e5c8cafef82cd6cdd27ab2f12b7dec22eba
thucln <thucln@vng.com.vn>
2022-02-23 19:49:11 +0700

    Update package

--- src/hooks/usePagination/index.ts
+++ src/hooks/usePagination/index.ts
@@ -1,25 +1,35 @@
 // @ts-ignore
-export const usePagination = ({ count, page, rowsPerPage, onPageChange }) => {
+export const usePagination = ({ page, totalRecords, rowsPerPage, onPageChange }) => {
+
+	const totalPages = Math.max(0, Math.ceil(totalRecords / rowsPerPage) - 1);
 
 	const handleFirstPageButtonClick = (event: any) => {
 		onPageChange(event, 0);
 	};
 
 	const handleBackButtonClick = (event: any) => {
+		if (page <= 0) return;
 		onPageChange(event, page - 1);
 	};
 
+	const handleNumberOfPageButtonClick = (event: any, numberOfPage: number) => {
+		onPageChange(event, numberOfPage);
+	}
+
 	const handleNextButtonClick = (event: any) => {
+		if (page + 1 > totalPages) return;
 		onPageChange(event, page + 1);
 	};
 
 	const handleLastPageButtonClick = (event: any) => {
-		onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+		onPageChange(event, totalPages);
 	};
 
 	return {
+		totalPages,
 		handleFirstPageButtonClick,
 		handleBackButtonClick,
+		handleNumberOfPageButtonClick,
 		handleNextButtonClick,
 		handleLastPageButtonClick
 	}
